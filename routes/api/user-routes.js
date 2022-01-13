8940be44ce5fdc7840f6d285bb42abb418ab891a
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2022-01-12 19:27:39 -0500

    created password incryption using bcrypt

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -20,9 +20,8 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     User.findOne({
             attributes: {
-                exclude: ['password']
-            },
-            where: {
+                exclude: ['password'] },
+                where: {
                 id: req.params.id
             }
         })
@@ -55,11 +54,40 @@ router.post('/', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+
+    // query operation
+
+    // expects {email, and password}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        // res.json({ user: dbUserData });
+
+        //verfiy user
+        const validPassword = dbUserData.checkPasssword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
     // pass in req.body instead to only update what's passed through
     User.update(req.body, {
+        individualHooks: true,
             where: {
                 id: req.params.id
             }
